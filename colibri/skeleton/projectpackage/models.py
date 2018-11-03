184445a3cf8bf3fa5abda94cc76ed9178c8ad2c9
Calin Crisan <ccrisan@gmail.com>
2018-11-03 23:46:00 +0200

    Add views example

---
+++ colibri/skeleton/projectpackage/models.py
@@ -0,0 +1,12 @@
+
+from colibri import persist
+
+#
+# Model example:
+#
+# class User(persist.Model):
+#     username = persist.CharField(max_length=128, index=True, unique=True)
+#     first_name = persist.CharField(max_length=64)
+#     last_name = persist.CharField(max_length=64)
+#     email = persist.CharField(max_length=128, null=True)
+#
