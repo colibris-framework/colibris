69b8605943362966c395afa038c0eb9e786d8ec8
Calin Crisan <ccrisan@gmail.com>
2018-11-04 16:22:57 +0200

    Example schema: inherit from ModelSchema

--- colibri/skeleton/projectpackage/schemas.py
+++ colibri/skeleton/projectpackage/schemas.py
@@ -1,18 +1,13 @@
 
-from marshmallow import Schema
-from marshmallow import fields, validate, pre_load, post_dump
+from colibri.schema import ModelSchema
+from marshmallow import fields, validate, pre_load
+
+from __packagename__ import models
 
 #
 # Schema example:
 #
-# class UserSchema(Schema):
-#     id = fields.Integer(dump_only=True)
-#     username = fields.String(required=True,
-#                              validate=validate.Length(min=6, max=128))
-#     first_name = fields.String(required=True,
-#                                validate=validate.Length(max=64))
-#     last_name = fields.String(required=True,
-#                               validate=validate.Length(max=64))
+# class UserSchema(ModelSchema):
 #     email = fields.String(validate=[validate.Email(error='Invalid email address.'),
 #                                     validate.Length(max=128)])
 #
@@ -22,11 +17,6 @@ from marshmallow import fields, validate, pre_load, post_dump
 #
 #         return data
 #
-#     @post_dump(pass_many=True)
-#     def wrap(self, data, many):
-#         key = 'users' if many else 'user'
-#
-#         return {
-#             key: data
-#         }
+#     class Meta:
+#         model = models.User
 #
