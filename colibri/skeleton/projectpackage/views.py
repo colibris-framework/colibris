7e46b3b22533cf1c28c30cb8548fea7b9a734ad6
Calin Crisan <ccrisan@gmail.com>
2018-11-04 16:24:07 +0200

    Example view: properly integrate with aiohttp_apispec

--- colibri/skeleton/projectpackage/views.py
+++ colibri/skeleton/projectpackage/views.py
@@ -1,7 +1,7 @@
 
 
 from aiohttp import web
-from aiohttp_apispec import docs
+from aiohttp_apispec import docs, use_kwargs, marshal_with
 
 from __packagename__ import models
 from __packagename__ import schemas
@@ -10,9 +10,11 @@ from __packagename__ import schemas
 # View example:
 #
 # @docs(summary='List all users')
+# @use_kwargs(schemas.UserSchema())
+# @marshal_with(schemas.UserSchema())
 # def list_users(request):
 #     users = models.User.select().order_by(models.User.username.asc())
-#     result = schemas.dump(list(users))
+#     result = schemas.UserSchema(many=True).dump(list(users))
 #
 #     return web.json_response(result)
 #
