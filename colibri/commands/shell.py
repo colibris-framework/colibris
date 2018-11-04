88a58f22960c395fa5d140c01c6e7466899692ca
Calin Crisan <ccrisan@gmail.com>
2018-11-04 16:14:59 +0200

    Automatically have models imported when running shell command

--- colibri/commands/shell.py
+++ colibri/commands/shell.py
@@ -1,13 +1,15 @@
 
 import code
+import importlib
+
+from colibri import settings
 
 from . import BaseCommand
 
 
 class ShellCommand(BaseCommand):
     def execute(self, options):
-        # stuff to pass to locals()
-        import colibri.persist
-        import colibri.settings
+        # have models automatically imported into locals()
+        models = importlib.import_module('{}.models'.format(settings.PROJECT_PACKAGE_NAME))
 
         code.interact(local=locals())
