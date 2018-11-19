bd637d8fc0799ed835bccfd775e8042b47876d1a
Calin Crisan <ccrisan@gmail.com>
2018-11-19 23:02:59 +0200

    Add app.get_health() sample

--- colibris/skeleton/__packagename__/app.py
+++ colibris/skeleton/__packagename__/app.py
@@ -1,5 +1,15 @@
 
+from colibris import app
+
 #
 # def init(webapp, loop):
 #     pass  # add your coroutines to the loop here
+#
+#
+# def get_health():
+#     # return your health status or raise app.HealthException()
+#
+#     return {
+#         'healthy': 'yes'
+#     }
 #
