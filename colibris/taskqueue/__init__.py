03b9c44ae43dc548cbad7d54b7ff7462a34d9867
Calin Crisan <ccrisan@gmail.com>
2018-12-09 20:38:55 +0200

    Add base task queue support

---
+++ colibris/taskqueue/__init__.py
@@ -0,0 +1,41 @@
+
+import logging
+
+from colibris import settings
+from colibris import utils
+
+
+DEFAULT_TIMEOUT = 300  # seconds
+
+logger = logging.getLogger(__name__)
+
+_backend = None
+
+
+def get_backend():
+    global _backend
+
+    if _backend is None:
+        backend_settings = dict(settings.TASK_QUEUE)
+        backend_path = backend_settings.pop('backend', None)
+        if backend_path is None:
+            return
+
+        backend_class = utils.import_member(backend_path)
+        _backend = backend_class(**backend_settings)
+
+        logger.debug('backend of class %s created', backend_path)
+
+    return _backend
+
+
+async def execute(func, *args, timeout=DEFAULT_TIMEOUT, **kwargs):
+    return await get_backend().execute(func, *args, timeout=timeout, **kwargs)
+
+
+def add_worker_arguments(parser):
+    get_backend().add_worker_arguments(parser)
+
+
+def run_worker(options):
+    get_backend().run_worker(options)
