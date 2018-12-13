fdd840d08b121801a9ba436893c223f7073aa7d7
Calin Crisan <ccrisan@gmail.com>
2018-12-13 17:09:59 +0200

    Schemas: expose validates_schema decorator

--- colibris/schemas.py
+++ colibris/schemas.py
@@ -2,7 +2,7 @@
 import marshmallow_peewee.schema
 
 from marshmallow import Schema, ValidationError
-from marshmallow import pre_dump, post_dump, pre_load, post_load
+from marshmallow import pre_dump, post_dump, pre_load, post_load, validates_schema
 from marshmallow import fields, validate
 
 from colibris.api import envelope
