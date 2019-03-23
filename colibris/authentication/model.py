1925184660f9b19d924709da4012f73bec7bd1e2
Calin Crisan <ccrisan@gmail.com>
2019-03-23 23:51:44 +0200

    Model auth backend: add active and inactive fields

--- colibris/authentication/model.py
+++ colibris/authentication/model.py
@@ -4,15 +4,23 @@ from colibris.authentication.base import AuthenticationBackend
 
 
 class ModelBackend(AuthenticationBackend):
-    def __init__(self, model, identity_field, secret_field, **kwargs):
+    def __init__(self, model, identity_field, secret_field, active_field=None, inactive_field=None, **kwargs):
         self.model = utils.import_member(model)
         self.identity_field = identity_field
         self.secret_field = secret_field
+        self.active_field = active_field
+        self.inactive_field = inactive_field
 
         super().__init__(**kwargs)
 
     def lookup_account(self, identity):
         query = self.get_identity_field() == identity
+        if self.active_field:
+            query = query & (self.get_active_field() == True)
+
+        if self.inactive_field:
+            query = query & (self.get_inactive_field() == False)
+
         try:
             return self.model.select().where(query).get()
 
@@ -21,6 +29,12 @@ class ModelBackend(AuthenticationBackend):
 
     def get_identity_field(self):
         return getattr(self.model, self.identity_field)
+
+    def get_active_field(self):
+        return getattr(self.model, self.active_field)
+
+    def get_inactive_field(self):
+        return getattr(self.model, self.inactive_field)
 
     def get_secret(self, account):
         return getattr(account, self.secret_field)
