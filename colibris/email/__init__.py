18a2287ab7289f9a8a68d200557faed76953b19d
Calin Crisan <ccrisan@gmail.com>
2019-06-02 23:38:09 +0300

    Use BackendMixin for emails

--- colibris/email/__init__.py
+++ colibris/email/__init__.py
@@ -2,36 +2,22 @@
 import logging
 
 from colibris import settings
-from colibris import utils
 
 from .message import EmailMessage
+from .base import EmailBackend
 
 
 logger = logging.getLogger(__name__)
 
-_backend = None
-
-
-def get_backend():
-    global _backend
-
-    if _backend is None:
-        backend_settings = dict(settings.EMAIL)
-        backend_path = backend_settings.pop('backend', 'colibris.email.base.NullBackend')
-        backend_class = utils.import_member(backend_path)
-
-        logger.debug('creating backend of class %s', backend_path)
-
-        _backend = backend_class(**backend_settings)
-
-    return _backend
-
 
 def send(email_message):
     logger.debug('sending %s', email_message)
-    return get_backend().send_messages([email_message])
+    return EmailBackend.get_instance().send_messages([email_message])
 
 
 def send_many(email_messages):
     logger.debug('sending %d messages', len(email_messages))
-    return get_backend().send_messages(email_messages)
+    return EmailBackend.get_instance().send_messages(email_messages)
+
+
+EmailBackend.configure(settings.EMAIL)
--- colibris/email/base.py
+++ colibris/email/base.py
@@ -1,18 +1,15 @@
 
 import logging
 
+from colibris.conf.backends import BackendMixin
+
 
 logger = logging.getLogger(__name__)
 
 
-class EmailBackend:
+class EmailBackend(BackendMixin):
     def __init__(self, **kwargs):
         pass
 
     def send_messages(self, email_messages):
         raise NotImplementedError
-
-
-class NullBackend(EmailBackend):
-    def send_messages(self, email_messages):
-        logger.debug('null backend: send_messages() called')
