9dd93e3d5d8d8c35fb6f8515246cb08e5ae6f165
Calin Crisan <ccrisan@gmail.com>
2019-06-08 22:47:53 +0300

    Only monkey-patch marshmallow.compat if missing

--- colibris/monkey.py
+++ colibris/monkey.py
@@ -1,24 +1,28 @@
 
-import marshmallow
-import sys
-import types
-
 
 # Patch marshmallow to provide marshmallow.compat which was removed in 3.0.0rc6.
 # Current latest version of marshmallow-peewee still uses marshmallow.compat.
 
-compat = types.ModuleType('compat')
-marshmallow.compat = compat
-sys.modules['marshmallow.compat'] = compat
+try:
+    from marshmallow import compat
+
+except ImportError:
+    import marshmallow
+    import sys
+    import types
+
+    compat = types.ModuleType('compat')
+    marshmallow.compat = compat
+    sys.modules['marshmallow.compat'] = compat
 
 
-def with_metaclass(meta, *bases):
-    class MetaClass(meta):
-        def __new__(cls, name, this_bases, d):
-            return meta(name, bases, d)
+    def with_metaclass(meta, *bases):
+        class MetaClass(meta):
+            def __new__(cls, name, this_bases, d):
+                return meta(name, bases, d)
 
-    return type.__new__(MetaClass, 'temporary_class', (), {})
+        return type.__new__(MetaClass, 'temporary_class', (), {})
 
 
-compat.with_metaclass = with_metaclass
-compat.PY2 = False
+    compat.with_metaclass = with_metaclass
+    compat.PY2 = False
