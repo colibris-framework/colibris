a83cc49446f164dee809bcac3d186a6f4f4848df
Calin Crisan <ccrisan@gmail.com>
2019-06-18 22:19:21 +0300

    Skeleton: make unhealthy test drop db instead of closing connection

--- colibris/skeleton/__packagename__/tests/test_health.py
+++ colibris/skeleton/__packagename__/tests/test_health.py
@@ -11,10 +11,10 @@ async def test_health_check_healthy(web_app_client):
 
 
 async def test_health_check_db_down(web_app_client):
-    persist.get_database().close()
+    persist.get_database().drop()
 
     resp = await web_app_client.get('/health')
     assert resp.status == 500
 
     j = await resp.json()
-    assert j == 'healthy'
+    assert j['code'] == 'unhealthy'
