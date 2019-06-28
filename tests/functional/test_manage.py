2138d306549402d5d9a73e9265e5e00041a98c1c
Calin Crisan <ccrisan@gmail.com>
2019-06-28 17:15:55 +0300

    Tests: add test_manage_migrate

--- tests/functional/test_manage.py
+++ tests/functional/test_manage.py
@@ -6,3 +6,10 @@ def test_manage_make_migrations(test_project):
     test_project.run_cmd('testproject/manage.py makemigrations initial')
 
     assert '001_initial.py' in os.listdir('testproject/migrations')
+
+
+def test_manage_migrate(test_project):
+    test_project.run_cmd('testproject/manage.py makemigrations initial')
+    test_project.run_cmd('testproject/manage.py migrate')
+
+    test_project.run_cmd('sqlite3 testproject.db .tables | grep user')
