aab793dfd898c69a7c20a8293e839c87bcdeb54b
Luci Furtun <lucianfurtun@gmail.com>
2019-07-11 21:43:12 +0300

    Use None as defaults for ModelView and APIView attributes

--- colibris/views.py
+++ colibris/views.py
@@ -8,7 +8,7 @@ from aiohttp_apispec import response_schema, request_schema
 
 from colibris.persist import Model
 from colibris.schemas import ModelSchema
-from marshmallow import ValidationError, Schema
+from marshmallow import ValidationError
 from peewee import IntegrityError
 
 from colibris import app, api
@@ -29,21 +29,21 @@ async def health(request):
 
 class _GenericMixinMeta(abc.ABCMeta):
     def __init__(cls, name, bases, attrs):
-        assert cls.model, 'The "model" field is required for {}.'.format(cls)
+        if getattr(cls, 'model', None) is not None:
+            assert issubclass(cls.model, Model) is True, 'The "model" should be a subclass of {}.'.format(Model)
 
-        assert issubclass(cls.model, Model) is True, 'The "model" should be a subclass of {}.'.format(Model)
+        if getattr(cls, 'body_schema_class', None) is not None:
+            if hasattr(cls, 'get'):
+                cls.get = response_schema(cls.body_schema_class)(cls.get)
 
-        if hasattr(cls, 'get'):
-            cls.get = response_schema(cls.body_schema_class)(cls.get)
+            if hasattr(cls, 'post'):
+                cls.post = request_schema(cls.body_schema_class)(response_schema(cls.body_schema_class)(cls.post))
 
-        if hasattr(cls, 'post'):
-            cls.post = request_schema(cls.body_schema_class)(response_schema(cls.body_schema_class)(cls.post))
+            if hasattr(cls, 'put'):
+                cls.put = request_schema(cls.body_schema_class)(response_schema(cls.body_schema_class)(cls.put))
 
-        if hasattr(cls, 'put'):
-            cls.put = request_schema(cls.body_schema_class)(response_schema(cls.body_schema_class)(cls.put))
-
-        if hasattr(cls, 'patch'):
-            cls.patch = request_schema(cls.body_schema_class)(response_schema(cls.body_schema_class)(cls.patch))
+            if hasattr(cls, 'patch'):
+                cls.patch = request_schema(cls.body_schema_class)(response_schema(cls.body_schema_class)(cls.patch))
 
         if getattr(cls, 'query_schema_class', None) is not None:
             for http_method in hdrs.METH_ALL:
@@ -73,11 +73,12 @@ class CreateMixin(metaclass=_GenericMixinMeta):
     body_schema_class = ModelSchema
 
     async def post(self):
+        query = self.get_query()
         schema = self.get_body_schema_class()
         data = await self.get_validated_body(schema)
-        print(data)
+
         try:
-            item = self.model.create(**data)
+            item = query.model.create(**data)
         except IntegrityError as err:
             traceback.print_exc()
             raise api.ServerError(code='server_error', message=str(err))
@@ -148,10 +149,12 @@ class DestroyMixin:
 
 
 class APIView(web.View):
-    body_schema_class = Schema
+    body_schema_class = None
     query_schema_class = None
 
     def get_body_schema_class(self, *args, **kwargs):
+        assert self.body_schema_class is not None, 'The attribute "body_schema_class" is required for {}'.format(self)
+
         kwargs.update({
             'context': {'request': self.request}
         })
@@ -161,8 +164,7 @@ class APIView(web.View):
         return schema
 
     def get_query_schema_class(self, *args, **kwargs):
-        if self.query_schema_class is None:
-            return None
+        assert self.query_schema_class is not None, 'The attribute "query_schema_class" is required for {}'.format(self)
 
         kwargs.update({
             'context': {'request': self.request}
@@ -200,8 +202,6 @@ class APIView(web.View):
         if schema is None:
             schema = self.get_query_schema_class()
 
-        assert schema is not None, 'The attribute "query_schema_class" is required for {}'.format(self)
-
         query = await self.get_request_query()
 
         try:
@@ -216,21 +216,26 @@ class APIView(web.View):
 
 
 class ModelView(APIView):
-    model = Model
-    body_schema_class = ModelSchema
+    model = None
+    body_schema_class = None
     url_identifier = 'id'
     lookup_field = 'id'
 
     def get_query(self):
+        assert self.model is not None, 'The attribute "model" is required for {}'.format(self)
+
         return self.model.select().order_by(self.model.id.desc())
 
     def get_object(self):
         identifier_value = self.request.match_info[self.url_identifier]
+        query = self.get_query()
+        model = query.model
 
         try:
-            instance = self.get_query().where(getattr(self.model, self.lookup_field) == identifier_value).get()
-        except self.model.DoesNotExist:
-            raise api.ModelNotFoundException(self.model)
+
+            instance = query.where(getattr(model, self.lookup_field) == identifier_value).get()
+        except query.model.DoesNotExist:
+            raise api.ModelNotFoundException(model)
 
         return instance
 
