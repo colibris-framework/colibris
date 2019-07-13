d2505febf76ccf4601144eade7eb34cff1e81315
Luci Furtun <lucianfurtun@gmail.com>
2019-07-13 22:45:23 +0300

    Refactor model views tests and add tests for pagination

--- tests/test_views/test_model_view.py
+++ tests/test_views/test_model_view.py
@@ -3,12 +3,11 @@ from aiohttp import web, hdrs
 from peewee import SqliteDatabase
 
 from colibris.middleware.errors import handle_errors_json
+from colibris.paginators import PageNumberPaginator
 from colibris.schemas import ModelSchema
 
 from colibris import persist
 from colibris.views.generic import ListCreateModelView, RetrieveUpdateDestroyModelView
-
-db = SqliteDatabase(':memory:')
 
 
 class Item(persist.Model):
@@ -23,6 +22,13 @@ class ItemSchema(ModelSchema):
 
 class ItemsView(ListCreateModelView):
     body_schema_class = ItemSchema
+    paginator_class = None
+    model = Item
+
+
+class ItemsPaginatedView(ListCreateModelView):
+    body_schema_class = ItemSchema
+    paginator_class = PageNumberPaginator
     model = Item
 
 
@@ -35,102 +41,159 @@ MODELS = [Item]
 
 
 @pytest.fixture
+def database():
+    db = SqliteDatabase(':memory:')
+    db.bind(MODELS)
+    db.connect()
+    db.create_tables(MODELS)
+
+
+@pytest.fixture
 def http_client(loop, aiohttp_client):
     middlewares = [
         handle_errors_json
     ]
 
     app = web.Application(middlewares=middlewares)
+    app.router.add_route('*', '/paginated-items', ItemsPaginatedView)
     app.router.add_route('*', '/items', ItemsView)
     app.router.add_route('*', '/items/{id}', ItemView)
     return loop.run_until_complete(aiohttp_client(app))
 
 
-def setup_module(module):
-    db.bind(MODELS)
+class TestList:
+    async def test_get_items(self, database, http_client):
+        response = await http_client.request(hdrs.METH_GET, "/items")
+        assert response.status == 200
 
-    db.connect()
-    db.create_tables(MODELS)
+        data = await response.json()
+        assert isinstance(data, list)
 
+    async def test_get_pagination_default_page_and_size(self, database, http_client):
+        item__name = 'Ligula Egestas Fermentum'
+        item_info = 'Ridiculus Fermentum Quam Porta'
 
-async def test_get_items(http_client):
-    response = await http_client.request(hdrs.METH_GET, "/items")
-    assert response.status == 200
+        Item.create(name=item__name, info=item_info)
 
+        response = await http_client.request(hdrs.METH_GET, "/paginated-items")
+        assert response.status == 200
 
-async def test_create_item(http_client):
-    item_name = 'Commodo Nibh'
-    item_info = 'Cras Lorem Purus Etiam Venenatis'
+        data = await response.json()
+        assert isinstance(data, dict)
+        assert 'results' in data
+        assert 'count' in data
+        assert 'pages' in data
+        assert 'page' in data
+        assert 'page_size' in data
 
-    response = await http_client.request(hdrs.METH_POST, "/items", json={'name': item_name, 'info': item_info})
+    async def test_get_pagination_different_page(self, database, http_client):
+        item_info = 'Ridiculus Fermentum Quam Porta'
+        first_item_name = 'Ligula Egestas Fermentum'
+        second_item_name = 'Euismod Ipsum Vulputate'
+        third_item_name = 'Pellentesque Sit Venenatis'
 
-    assert response.status == 201
+        Item.create(name=first_item_name, info=item_info)
+        Item.create(name=second_item_name, info=item_info)
+        Item.create(name=third_item_name, info=item_info)
 
-    item = Item.select().where(Item.name == item_name).get()
+        response = await http_client.request(hdrs.METH_GET, "/paginated-items", params={'page_size': 1, 'page': 1})
+        assert response.status == 200
 
-    assert item.name == item_name
+        data = await response.json()
+        assert isinstance(data, dict)
+        assert data['count'] == 3
+        assert data['page'] == 1
+        assert data['page_size'] == 1
+        assert data['results'][0]['name'] == third_item_name
 
+        response = await http_client.request(hdrs.METH_GET, "/paginated-items", params={'page_size': 1, 'page': 2})
+        assert response.status == 200
+        data = await response.json()
+        assert data['results'][0]['name'] == second_item_name
 
-async def test_create_item_validation(http_client):
-    response = await http_client.request(hdrs.METH_POST, "/items")
+        response = await http_client.request(hdrs.METH_GET, "/paginated-items", params={'page_size': 1, 'page': 3})
+        assert response.status == 200
+        data = await response.json()
+        assert data['results'][0]['name'] == first_item_name
 
-    assert response.status == 400
-    data = await response.json()
-    assert 'name' in data['details']
-    assert 'info' in data['details']
-
-
-async def test_update_item(http_client):
-    item_initial_name = 'Ligula Egestas Fermentum'
-    item_updated_name = 'Cursus Inceptos'
-    item_info = 'Ridiculus Fermentum Quam Porta'
-
-    item = Item.create(name=item_initial_name, info=item_info)
-
-    response = await http_client.request(hdrs.METH_PATCH, f"/items/{item.id}", json={'name': item_updated_name})
-
-    assert response.status == 200
-
-    updated_item = Item.select().where(Item.name == item_updated_name).get()
-
-    assert item.id == updated_item.id
-
-
-async def test_update_item_validation(http_client):
-    item_initial_name = 'Nibh Lorem Amet Aenean'
-    item_info = 'Ridiculus Fermentum Quam Porta'
-
-    item = Item.create(name=item_initial_name, info=item_info)
-
-    response = await http_client.request(hdrs.METH_PATCH, f"/items/{item.id}", json={'name': None})
-
-    assert response.status == 400
-
-    data = await response.json()
-    assert 'name' in data['details']
+        response = await http_client.request(hdrs.METH_GET, "/paginated-items", params={'page_size': 1, 'page': 4})
+        assert response.status == 200
+        data = await response.json()
+        assert data['results'] == []
 
 
-async def test_update_item_not_found(http_client):
-    response = await http_client.request(hdrs.METH_PATCH, f"/items/11011", json={'name': 'Egestas Fringilla'})
+class TestCreate:
+    async def test_create_item(self, database, http_client):
+        item_name = 'Commodo Nibh'
+        item_info = 'Cras Lorem Purus Etiam Venenatis'
 
-    assert response.status == 404
+        response = await http_client.request(hdrs.METH_POST, "/items", json={'name': item_name, 'info': item_info})
+
+        assert response.status == 201
+
+        item = Item.select().where(Item.name == item_name).get()
+
+        assert item.name == item_name
+
+    async def test_create_item_validation(self, database, http_client):
+        response = await http_client.request(hdrs.METH_POST, "/items")
+
+        assert response.status == 400
+        data = await response.json()
+        assert 'name' in data['details']
+        assert 'info' in data['details']
 
 
-async def test_delete_item(http_client):
-    item_name = 'Sit Lorem'
-    item_info = 'Ridiculus Fermentum Quam Porta'
+class TestUpdate:
+    async def test_update_item(self, database, http_client):
+        item_initial_name = 'Ligula Egestas Fermentum'
+        item_updated_name = 'Cursus Inceptos'
+        item_info = 'Ridiculus Fermentum Quam Porta'
 
-    item = Item.create(name=item_name, info=item_info)
+        item = Item.create(name=item_initial_name, info=item_info)
 
-    response = await http_client.request(hdrs.METH_DELETE, f"/items/{item.id}")
+        response = await http_client.request(hdrs.METH_PATCH, f"/items/{item.id}", json={'name': item_updated_name})
 
-    assert response.status == 204
+        assert response.status == 200
 
-    items = Item.select().where(Item.name == item_name)
-    assert items.count() == 0
+        updated_item = Item.select().where(Item.name == item_updated_name).get()
+
+        assert item.id == updated_item.id
+
+    async def test_update_item_validation(self, database, http_client):
+        item_initial_name = 'Nibh Lorem Amet Aenean'
+        item_info = 'Ridiculus Fermentum Quam Porta'
+
+        item = Item.create(name=item_initial_name, info=item_info)
+
+        response = await http_client.request(hdrs.METH_PATCH, f"/items/{item.id}", json={'name': None})
+
+        assert response.status == 400
+
+        data = await response.json()
+        assert 'name' in data['details']
+
+    async def test_update_item_not_found(self, database, http_client):
+        response = await http_client.request(hdrs.METH_PATCH, f"/items/11011", json={'name': 'Egestas Fringilla'})
+
+        assert response.status == 404
 
 
-async def test_delete_item_not_found(http_client):
-    response = await http_client.request(hdrs.METH_DELETE, f"/items/111111")
+class TestDestroy:
+    async def test_delete_item(self, database, http_client):
+        item_name = 'Sit Lorem'
+        item_info = 'Ridiculus Fermentum Quam Porta'
 
-    assert response.status == 404
+        item = Item.create(name=item_name, info=item_info)
+
+        response = await http_client.request(hdrs.METH_DELETE, f"/items/{item.id}")
+
+        assert response.status == 204
+
+        items = Item.select().where(Item.name == item_name)
+        assert items.count() == 0
+
+    async def test_delete_item_not_found(self, database, http_client):
+        response = await http_client.request(hdrs.METH_DELETE, f"/items/111111")
+
+        assert response.status == 404
