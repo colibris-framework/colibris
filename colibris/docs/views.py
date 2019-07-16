640d9ac1ee16759b9536d75205ca821d33fefb09
Luci Furtun <lucianfurtun@gmail.com>
2019-07-16 23:06:06 +0300

    Refactor swagger_view function

--- colibris/docs/views.py
+++ colibris/docs/views.py
@@ -7,12 +7,12 @@ from colibris.docs import STATIC_PATH, APISPEC_URL, STATIC_URL
 
 async def swagger_view(request):
     with open(join(STATIC_PATH, "index.html")) as f:
-        content = f.read().replace("##API_SPEC_URL##", APISPEC_URL).replace("##STATIC_BASE_URL##", STATIC_URL)
+        content = f.read()
 
-    return Response(
-        text=content,
-        content_type="text/html"
-    )
+    content = content.replace("##API_SPEC_URL##", APISPEC_URL)
+    content = content.replace("##STATIC_BASE_URL##", STATIC_URL)
+
+    return Response(text=content, content_type="text/html")
 
 
 async def apispec_view(request):
