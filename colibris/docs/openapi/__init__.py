fc535afafb6d9312d413dc66271971c3958e8059
Luci Furtun <lucianfurtun@gmail.com>
2019-07-17 22:06:12 +0300

    Rename API_DOCS_PATH -> API_DOCS_URL

--- colibris/docs/openapi/__init__.py
+++ colibris/docs/openapi/__init__.py
@@ -4,6 +4,6 @@ from colibris.conf import settings
 
 STATIC_PATH = abspath(join(dirname(__file__), 'swagger'))
 
-UI_URL = settings.API_DOCS_PATH
+UI_URL = settings.API_DOCS_URL
 STATIC_URL = '{}/static'.format(UI_URL)
 APISPEC_URL = '{}/apispec'.format(UI_URL)
