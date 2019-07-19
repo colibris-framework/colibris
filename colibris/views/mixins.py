73a9f106d5a4d3f871fbff19a0e1ba6747ce538b
Luci Furtun <lucianfurtun@gmail.com>
2019-07-19 15:20:53 +0300

    Use inspect.isawaitable instead of inspect.iscoroutinefunction

--- colibris/views/mixins.py
+++ colibris/views/mixins.py
@@ -11,17 +11,17 @@ class ListMixin:
         if self.pagination_class is not None:
             return await self._get_paginated_response(query, schema)
 
-        result = schema.dump(list(query))
+        data = schema.dump(list(query))
 
-        return web.json_response(result)
+        return web.json_response(data)
 
     async def _get_paginated_response(self, query, schema):
         paginator = self.pagination_class(query, self.request)
         paginated_query = paginator.paginate_query()
-        result = schema.dump(list(paginated_query))
-        paginated_result = paginator.get_enveloped_data(result)
+        data = schema.dump(list(paginated_query))
+        paginated_data = paginator.get_enveloped_data(data)
 
-        return web.json_response(paginated_result)
+        return web.json_response(paginated_data)
 
 
 class CreateMixin:
@@ -29,20 +29,31 @@ class CreateMixin:
         schema = self.get_body_schema()
         data = await self.get_validated_body(schema)
 
-        if inspect.iscoroutinefunction(self.perform_create):
-            instance = await self.perform_create(data)
+        result = self.perform_create(data)
+        if inspect.isawaitable(result):
+            instance = await result
         else:
-            instance = self.perform_create(data)
+            instance = result
 
-        result = schema.dump(instance)
+        data = schema.dump(instance)
 
-        return web.json_response(result, status=201)
+        return web.json_response(data, status=201)
 
     def perform_create(self, data):
         query = self.get_query()
         instance = query.model.create(**data)
 
         return instance
+
+
+class RetrieveMixin:
+    async def get(self):
+        schema = self.get_body_schema()
+        instance = self.get_object()
+
+        data = schema.dump(instance)
+
+        return web.json_response(data)
 
 
 class UpdateMixin:
@@ -58,14 +69,15 @@ class UpdateMixin:
         schema = self.get_body_schema(partial=partial, instance=instance)
         data = await self.get_validated_body(schema)
 
-        if inspect.iscoroutinefunction(self.perform_update):
-            instance = await self.perform_update(data, instance)
+        result = self.perform_update(data, instance)
+        if inspect.isawaitable(result):
+            instance = await result
         else:
-            instance = self.perform_update(data, instance)
+            instance = result
 
-        result = schema.dump(instance)
+        data = schema.dump(instance)
 
-        return web.json_response(result)
+        return web.json_response(data)
 
     def perform_update(self, data, instance):
         instance.update_fields(data)
@@ -78,22 +90,13 @@ class DestroyMixin:
     async def delete(self):
         instance = self.get_object()
 
-        if inspect.iscoroutinefunction(self.perform_destroy):
-            await self.perform_destroy(instance)
-        else:
-            self.perform_destroy(instance)
+        result = self.perform_destroy(instance)
+        if inspect.isawaitable(result):
+            await result
 
         return web.json_response(status=204)
 
     def perform_destroy(self, instance):
         instance.delete_instance()
 
-
-class RetrieveMixin:
-    async def get(self):
-        schema = self.get_body_schema()
-        instance = self.get_object()
-
-        result = schema.dump(instance)
-
-        return web.json_response(result)
+        return None
