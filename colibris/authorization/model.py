f74e426269c680f65bc54b651b314ccda81b9358
Calin Crisan <ccrisan@gmail.com>
2019-07-26 10:34:26 +0300

    Auth model backends: allow specifying the model class directly

--- colibris/authorization/model.py
+++ colibris/authorization/model.py
@@ -6,7 +6,10 @@ from .base import AuthorizationBackend
 
 class ModelBackend(AuthorizationBackend):
     def __init__(self, model, account_field, **kwargs):
-        self.model = utils.import_member(model)
+        self.model = model
+        if isinstance(model, str):
+            self.model = utils.import_member(self.model)
+
         self.account_field = account_field
 
         super().__init__(**kwargs)
