36aa6aa3c547da5fad9d3215a9b2dd5676442f1c
amecea <flavius@presslabs.com>
2020-10-26 15:44:37 +0200

    Constraint jwt verify to a list of algorithms

--- colibris/authentication/jwt.py
+++ colibris/authentication/jwt.py
@@ -71,10 +71,13 @@ class JWTBackend(ModelBackend, CookieBackendMixin):
         secret = self.get_secret(account)
 
         try:
-            jwt.decode(auth_data['token'], key=secret, verify=True)
+            jwt.decode(auth_data['token'], key=secret, verify=True, algorithms=[JWT_ALG])
 
         except jwt.InvalidSignatureError:
             raise JWTException('invalid signature')
+
+        except jwt.InvalidAlgorithmError:
+            raise JWTException("invalid algorithm")
 
         if self.is_csrf_enabled():
             self.verify_csrf(request, account)
