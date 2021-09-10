e9e18cece822b87a7eaadda79b3a80975edf3ed9
Emanuel Danci <danci.emanuel@gmail.com>
2021-09-10 13:22:41 +0300

    Restrict async-timeout version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         'aiohttp>=3.5.0,<3.7',
         'aiohttp-apispec>=1.5',
         'apispec>=3.0.0,<4.0.0',
-        'async-timeout',
+        'async-timeout<4.0',
         'marshmallow>=3.0.0b19,<3.8',
         'marshmallow_peewee>=2.3.0,<3.1',
         'peewee>=3.9',
